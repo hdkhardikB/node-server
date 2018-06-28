7c9a7a9b3e3299b79b0384af978f1d0672d71118
Hardik Babaria <hdk@HDK.local>
2018-06-28 23:28:29 +0530

    * Changed home path.

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/bacancy-app/dist/'));
 
 //Defining separate routes for every modules.
 app.use('/', routes);
